aa0a2cff4ff906f684d3bfd06dee44e0b2aad0fc
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2024-09-12 10:20:33 -0400

    Completed To-Dos and CoffeeDrink Class

---
+++ src/CoffeeDrink.java
@@ -0,0 +1,31 @@
+// CoffeeDrink object class
+
+class CoffeeDrink {
+
+    int numberOfCups;
+    String flavor;
+    boolean hasWhippedCream;
+
+    // Constructor for CoffeeDrink class
+    public CoffeeDrink(int numberOfCups, String flavor, boolean hasWhippedCream) {
+        // TODO
+        // Edit this constructor to accept three parameters,
+        // one for each CoffeeDrink variable.
+        // Assign those parameter values to the class-wide variables.
+        this.numberOfCups = numberOfCups;
+        this.flavor = flavor;
+        this.hasWhippedCream = hasWhippedCream;
+
+    }
+
+    // Method to print information about the CoffeeDrink
+    public void printInfo() {
+        // TODO
+        // Make this method print order details.
+        // Include information stored in each variable.
+        System.out.println("Order Details:");
+        System.out.println("Number of Cups: " + numberOfCups);
+        System.out.println("Flavor: " + flavor);
+        System.out.println("Whipped Cream: " + (hasWhippedCream ? "Yes" : "No"));
+    }
+}
---
+++ src/JavaBeansAndBytes.java
@@ -0,0 +1,122 @@
+import java.util.Random;
+
+/**
+ * CS2 Review Assignment!
+ * Do NOT use ChatGPT or any other generative AI to assist you with this assignment
+ * You may reference your own past work and the internet (not including AI)
+ */
+
+public class JavaBeansAndBytes {
+
+    // Variables for the CoffeeShop
+    String name;
+    boolean isOpen;
+    int yearFounded;
+
+    public static void main(String[] args) {
+        // Create a new CoffeeShop instance to run everything.
+        JavaBeansAndBytes cafe = new JavaBeansAndBytes();
+    }
+
+    // Constructor for CoffeeShop
+    public JavaBeansAndBytes() {
+        name = "Java Beans & Bytes: The Code Café";
+        isOpen = false;
+        yearFounded = 1995;
+
+        System.out.println("Welcome to " + name + "!");
+        if (isOpen) {
+            System.out.println("We are open!");
+        } else {
+            System.out.println("We are closed :(");
+        }
+        System.out.println("We were founded in " + yearFounded + ".");
+
+        // TODO
+        // Update the year founded and print the new yearFounded in a sentence
+        yearFounded = 2000;
+        System.out.println("We were founded in " + yearFounded + ".");
+        // Calling methods
+        randomDiscount();
+        // TODO
+        // Add parameter to specialOfTheDay (drink of your choice)
+        specialOfTheDay("Milk");
+        countCups();
+        baristasChoice();
+
+        // TODO
+        // Create two CoffeeDrink objects
+        CoffeeDrink coffee1 = new CoffeeDrink(2, "Caramel", true);
+        CoffeeDrink coffee2 = new CoffeeDrink(1, "Vanilla", false);
+        // TODO
+        // Print out the order details
+        coffee1.printInfo();
+        coffee2.printInfo();
+
+
+    }
+
+    // Method to generate a random discount
+    public void randomDiscount() {
+        // TODO
+        // Make this method generate a random integer discount (0-30%)
+        // and print the result.
+        int discount = (int)(Math.random()*31);
+        System.out.println("You have received a " + discount + "% discount today");
+    }
+
+    // Method with a parameter for the special of the day
+    public void specialOfTheDay(String special) {
+        // TODO
+        // Make this method accept a parameter representing the day’s special.
+        // Print the day's special.
+        System.out.println("Today's special is " + special);
+    }
+
+    // Method to show loops
+    public void countCups() {
+        System.out.println("Counting cups sold today:");
+
+        // TODO
+        // Write three different for loops that print the indicated numbers:
+        // Print 1 to 5
+        for(int i = 1; i<=5; i++){
+            System.out.print(i);
+        }
+        System.out.println("");
+        // Print 2, 5, 8, 11
+        for(int z = 2; z<=11; z = z + 3){
+            System.out.print(z);
+            if(z<11){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("");
+        // Print 8 to 0
+        for(int f = 8; f>=0; f = f - 1){
+            System.out.print(f);
+        }
+    }
+
+    // Method to recommend a coffee based on a random number
+    public void baristasChoice() {
+        // TODO
+        // Make this method generate a random decimal between 0 and 1
+        System.out.println();
+        double bradford = Math.random();
+        if(bradford < .25){
+            System.out.println("water");
+        }
+        else if(bradford < .50){
+            System.out.println("Chocolate Milk");
+        }
+        else if (bradford < .75){
+            System.out.println("Apple Juice");
+        }
+        else{
+            System.out.println("Lemonade");
+        }
+        // and print one of four drink recommendations based on its value.
+    }
+}
+
